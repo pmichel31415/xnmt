688d66c3e73fbdf39a0fa0c97ad19acd5cf49678
Mingxing DU <fdsyen@gmail.com>
2017-07-21 17:04:13 -0400

    changes to get hidden states

--- xnmt/reports.py
+++ xnmt/reports.py
@@ -61,7 +61,7 @@ class DefaultTranslatorReport(object):
       if self.trg_text != None: f.write("<p><b>Target Text: </b> {}</p>\n".format(self.trg_text))
       if self.trg_words != None: f.write("<p><b>Target Words: </b> {}</p>\n".format(' '.join(self.trg_words)))
       # Alignments
-      if  all([type(x) != None for x in [self.src_words, self.trg_words, self.attentions]]):
+      if  all([x != None for x in [self.src_words, self.trg_words, self.attentions]]):
         if type(self.attentions) == dy.Expression:
           self.attentions = self.attentions.npvalue()
         elif type(self.attentions) == list:
@@ -73,8 +73,8 @@ class DefaultTranslatorReport(object):
         f.write("<p><b>Attention:</b><br/><img src=\"{}.attention.png\"/></p>\n".format(filename_of_report))
 
       f.write("</body></html>")
-
-    np.save("{}.hidden_states".format(path_to_report) , self.hidden_states[0],npvalue())
+    print('\ndynet expression of hidden states', self.hidden_states[0], '\n')
+    print("\n{}.hidden_states".format(path_to_report) , self.hidden_states[0].npvalue(), '\n')
 
 if __name__ == "__main__":
 
